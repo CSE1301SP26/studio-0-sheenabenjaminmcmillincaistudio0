33486ef86a6e862fddca7c2fca61c6b51c732d70
emcmillin06 <e.j.mcmillin@wustl.edu>
2026-01-14 14:06:04 -0600

    done

--- src/studio0/Message.java
+++ src/studio0/Message.java
@@ -3,6 +3,7 @@ package studio0;
 public class Message {
 
 	public static void main(String args[]) {
-		System.out.println(" theasdl;fk   l;k j workshop! ");
+		System.out.println(" theasdl;fk  end of studio zero");
 	}   
+	
 }
